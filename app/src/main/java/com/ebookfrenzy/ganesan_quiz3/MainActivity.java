f958b49103451ecc388514d152812f14619522e7
user <ganesankohilan007@gmail.com>
2021-02-01 15:56:50 +0800

    final

---
+++ app/src/main/java/com/ebookfrenzy/ganesan_quiz3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ebookfrenzy.ganesan_quiz3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://google.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
